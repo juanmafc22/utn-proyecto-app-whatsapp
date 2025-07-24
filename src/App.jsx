f75bd4790751bf7a1c48057fc2cb4c3a4712f54b
Juan Manuel Fernandez <juanmafc22@gmail.com>
2025-07-24 16:24:10 -0300

    Agregados los elementos tentativos del NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,51 @@ import React from 'react'
 
 import { useState } from 'react'
 
-function App() {
+import NavBar from './Components/NavBar/NavBar'
 
+function App() {
   return (
     <div>
-      <h1>Hello</h1>
-
+      <ComponenteNombre />
+      <ComponenteJuan />
+      <ComponenteManuel />
+      <ComponenteFernandez />
+      <NavBar />
     </div>
   )
+}
 
 
+function ComponenteNombre() {
+  return (
+    <div>
+      <h1>Este es el título de los nombre</h1>
+    </div>
+  )
+}
+
+function ComponenteJuan() {
+  return (
+    <div>
+      <p>Juan</p>
+    </div>
+  )
+}
+
+function ComponenteManuel() {
+  return (
+    <div>
+      <p>Manuel</p>
+    </div>
+  )
+}
+
+function ComponenteFernandez() {
+  return (
+    <div>
+      <p>Fernandez</p>
+    </div>
+  )
 }
 
 export default App
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+/*
+Elementos tentativos del Nav Bar:
+- Arriba de todo
+    - Burger menu
+    - Chats
+    - Estados
+    - Llamados
+- Abajo (separado por una línea delgada gris)
+    - Rueda de ajustes
+    - Perfil
+*/
+
+function NavBar() {
+
+    return (
+        <div>
+            <p>Este es la navbar</p>
+        </div>
+    )
+
+}
+
+export default NavBar
